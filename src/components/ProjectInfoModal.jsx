aceab3d4ce062c6019f6ac49bc2372fb3b4e199c
Christina Vekri <christina.vek@gmail.com>
2024-11-06 16:34:01 +0100

    modal text size fix

--- src/components/ProjectInfoModal.jsx
+++ src/components/ProjectInfoModal.jsx
@@ -10,7 +10,7 @@ export const ProjectInfoModal = ({ project, handleCloseCard }) => {
         </button>
         <div className="flex-1 mb-4 md:mb-0 md:mr-4 flex items-center justify-center">
           <iframe
-            className="w-full h-full md:w-[800px] md:h-[537px] aspect-w-16 aspect-h-9"
+            className="w-full h-56 sm:h-64 md:w-[700px] md:h-[400px] lg:w-[800px] lg:h-[450px]"
             src={project.video}
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
@@ -18,10 +18,10 @@ export const ProjectInfoModal = ({ project, handleCloseCard }) => {
           />
         </div>
         <div className="flex-1 flex flex-col justify-center text-center md:text-left">
-          <h3 className="text-3xl text-white font-bold mb-4">
+          <h3 className="text-xl sm:text-2xl text-white font-bold mb-4">
             {project.title}
           </h3>
-          <p className="text-white text-lg">{project.info}</p>
+          <p className="text-white text-base sm:text-lg text-lg">{project.info}</p>
         </div>
       </div>
     </div>
